f6993e41f9b0ab1340f842304b3e5dc3da133ce9
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-04-05 16:45:27 -0300

    connection to sqlserver

---
+++ CRUD_DB_Empleados/Data/SQLServers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace CRUD_DB_Empleados.Data
+{
+    class SQLServers
+    {
+        SqlConnection connection;
+
+        //con este metodo vamos a conectarnos a la base de datos
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                connection = new SqlConnection("Server=DESKTOP-GJ9RV1H\\SQLEXPRESS;" +
+                                           "Database=Empleado;integrated security=true");
+                connection.Open();
+                return connection;
+            }
+            catch(Exception e)
+            {
+
+                throw new Exception("Ha ocurrido un error al conectarse " + e.Message);
+            }
+
+        }
+
+
+
+        //con este metodo vamos a desconectarnos
+        public bool Disconnect()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw new Exception("Ha ocurrido un error al desconectase " + e.Message);
+                return false;
+            }
+        }
+
+    }
+}
