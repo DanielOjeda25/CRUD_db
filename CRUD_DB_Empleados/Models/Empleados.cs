5341eb603f43383af6e4ceee129421a73b70ab37
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-04-05 16:46:41 -0300

    entity file

---
+++ CRUD_DB_Empleados/Models/Empleados.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_DB_Empleados.Models
+{
+
+//en esta clase tendremos almacenados los atributos de las entidades
+//en este caso tomando los mismos datos de la tabla empleados
+    internal class Empleados
+    {
+
+        //atributos
+        private string document;
+        private string names;
+        private string surname;
+        private int age;
+        private string direction;
+        private string dateofbirth;
+
+        
+        
+        //Atributos inicializados
+        public Empleados()
+        {
+            this.document = "";
+            this.names = "";
+            this.surname = "";
+            this.age = 0;
+            this.direction = "";
+            this.dateofbirth = "";
+        }
+
+        //Setters y Getters
+        public string Document { get => document; set => document = value; }
+        public string Names { get => names; set => names = value; }
+        public string Surname { get => surname; set => surname = value; }
+        public int Age { get => age; set => age = value; }
+        public string Direction { get => direction; set => direction = value; }
+        public string Dateofbirth { get => dateofbirth; set => dateofbirth = value; }
+    }
+}
