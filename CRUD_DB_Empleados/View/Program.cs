a40bee2f1d2072966da230b37510b4dd5297e485
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2022-04-05 16:47:22 -0300

    main

---
+++ CRUD_DB_Empleados/View/Program.cs
@@ -0,0 +1,16 @@
+namespace CRUD_DB_Empleados
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+           
+            ApplicationConfiguration.Initialize();
+            Application.Run(new CrudForm());
+        }
+    }
+}
